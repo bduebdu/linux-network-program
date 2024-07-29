e180c6852d261454fd9b8902166a1b5d792ad801
bduebdu <87608965+bduebdu@users.noreply.github.com>
2024-07-29 09:30:27 +0800

    0729

---
+++ IPC/fifo/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+int main()
+{
+    //创建进程
+    pid_t pid= fork();
+
+
+
+    return 0;
+}
