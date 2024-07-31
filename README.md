d72d2f0cf3786203e2cc4224b858420bbd9d7adc
bduebdu <87608965+bduebdu@users.noreply.github.com>
2024-07-31 17:59:49 +0800

    0731

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 0725 I/O
 0727 process/IPC
+0731 thread/threadSync
---
+++ threadSync/conditionVary.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    
+
+
+
+
+    return 0;
+}
